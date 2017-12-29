8a640a392dc9b479c8f6d15879ec9eaf6feefa9f
Arsenic <kamil.k.mat@gmail.com>
2017-12-29 21:39:06 +0100

    Adding function to set properties

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace Mymovielist;
 
+use Mockery\Exception;
 use Mymovielist\NEO4J\NEO4JUser;
 use Mymovielist\SQL\SQLUser;
 
@@ -12,13 +13,48 @@ class User
     {
         NEO4JUser::create(['login' => $data['login']]);
 
-        return SQLUser::create([
-            'login' => $data['login'],
-            'email' => $data['email'],
-            'password' => $data['password'],
-            'access' => 'u'
-        ]);
+        return SQLUser::create(
+            [
+                'login'    => $data['login'],
+                'email'    => $data['email'],
+                'password' => $data['password'],
+                'access'   => 'u'
+            ]
+        );
 
+
+    }
+
+    public static function setAttribute($id,$attribute,$value)
+    {
+        $user = SQLUser::where('id', $id);
+
+        switch ($attribute)
+        {
+            case "name":
+                $user->update(['name' => $value]);
+                break;
+            case "surname":
+                $user->update(['surname' => $value]);
+                break;
+            case "avatar":
+                $user->update(['avatar' => $value]);
+                break;
+            case "birthday":
+                $user->update(['birthday' => $value]);
+                break;
+            case "about":
+                $user->update(['about' => $value]);
+                break;
+            case "location":
+                $user->update(['location' => $value]);
+                break;
+            case "gender":
+                $user->update(['gender' => $value]);
+                break;
+            default:
+                throw new Exception("Wrong argument passed");
+        }
 
     }
 
